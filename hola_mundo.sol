32a3d631e4abc1aa6f803f997312ae812c9c175d
Oriojas <oscarriojas@gmail.com>
2024-10-08 19:16:29 -0500

    primer contrato

---
+++ hola_mundo.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// 0x9a47f8ab99e78617B8AD09D767b32F1bF9C2246d
+
+pragma solidity 0.8.24;  // en esta parte definoi la version del compilador
+
+// a partir de aca coloco la logica de mi contrato
+contract hola_mundo {
+
+    string public saludo = "Hola desde Educateth 2.0"; // declarando el contenido de mi variable saludo
+
+}
